786c44f7f06c686ecfe10d350b31c85c97165757
stevenke <stevenke@tencent.com>
2020-10-23 20:24:22 +0800

    Initial commit

---
+++ din/din_model.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@version:
+@author: kx
+@file: din_model.py
+@function:
+@modify:
+@reference: https://github.com/R-Stalker/RSDeepModel/blob/master/rsdeepmodel/models/din.py
+"""
+
+
+
+if __name__ == "__main__":
+    pass
